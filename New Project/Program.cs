80437322ff62f24806342202d088f5611833bbaf
kop4ikoid <kop4ikoid@gmail.com>
2015-10-24 00:16:27 +0300

    Created by Coding Ground Client

---
+++ New Project/Program.cs
@@ -0,0 +1,33 @@
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main( string [] args)
+        {
+            Point p1=new Point();
+            p1.x=1;
+            p1.y=3;
+            p1.sym='*';
+            p1.Draw()
+
+            Point p1=new Point();
+            p2.x=4;
+            p2.y=5;
+            p2.sym='#';
+            p2.Draw()
+
+            Console.SetCursorPosition(x2,y2);
+            Console.Write(sym2);
+            
+            Console.ReadLine();
+        }
+        
+    }
+}
